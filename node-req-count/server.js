032db9f1b56d64163727a3a3de078015c1aee11a
Amera Ayad <amera.ayad.it@gmail.com>
2019-12-23 10:09:17 +0100

    sa

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -8,10 +8,32 @@ var server = http.createServer(function(request, response) {
   var endpoint = url.parse(request.url, true).pathname;
   var property = endpoint.replace(/^\//, '');
 
+  response.writeHead(200);
+
   if (request.method === 'POST') {
     // YOUR CODE HERE
+    if(globalCounter[property] !== undefined){
+        console.log(globalCounter[property], '999')
+        globalCounter[property] = globalCounter[property] + 1;
+      
+      // {
+      //   'Content-Type': 'text/plain' 
+      // }
+      response.end();
+    } else if(globalCounter[property] === undefined){
+        console.log(globalCounter[property], '555')
+        globalCounter[request.method] = 0;
+      
+      response.end();
+    }
   } else if (request.method === 'GET') {
     // YOUR CODE HERE
+    
+    // if(globalCounter[property]){
+      // response.statusCode = 200;
+      console.log(property, globalCounter[property])
+      response.end(JSON.stringify({ count: globalCounter[property]}));
+    // } 
   } else {
     response.statusCode = 404;
     response.end();
