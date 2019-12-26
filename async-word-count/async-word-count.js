31113241ffd5c01c7ecde1ab368b8983b5be3db7
Amera Ayad <amera.ayad.it@gmail.com>
2019-12-26 15:10:46 +0100

    aa

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -7,14 +7,35 @@ var getWordCount = function(filePath, callback) {
       callback(err, null);
       return;
     }
-
     var wordCount = data.trim().split(' ').length;
     callback(null, wordCount);
   });
 };
 
-var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
-  // YOUR CODE HERE
+var getTotalWordCount = function(
+  filePathOne , 
+  filePathTwo , 
+  callback) {
+
+  getWordCount(filePathOne, function(err, count){
+
+    if(err){
+      return callback(err, null);
+    }
+    getWordCount(filePathTwo, function(err, count2){
+      if(err){
+        return callback(err, null);
+      }
+      callback(err, count + count2);
+    });
+
+  });
+
 };
+
+//function call
+getTotalWordCount(path.join(__dirname, 'files/fileOne.txt'),path.join(__dirname, 'files/fileTwo.txt'), (err, total) => {
+  console.log(total);
+});
 
 module.exports = getTotalWordCount;
